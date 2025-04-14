915b58d496e806975769e228c71fd8638c2e5b96
AmiR <www.devmoon@gmail.com>
2025-04-14 22:43:54 +0330

    Update blocker.js

--- blocker.js
+++ blocker.js
@@ -1,40 +1,36 @@
-document.addEventListener('contextmenu', event => event.preventDefault());
-jQuery(document).ready(function () {
+document.addEventListener('DOMContentLoaded', function () {
     function disableSelection(e) {
-        if (typeof e.onselectstart != "undefined") e.onselectstart = function () {
-            return false
-        };
-        else if (typeof e.style.MozUserSelect != "undefined") e.style.MozUserSelect = "none";
-        else e.onmousedown = function () {
-            return false
-        };
-        e.style.cursor = "default"
+        e.onselectstart = () => false;
+        e.style.userSelect = "none";
+        e.onmousedown = () => false;
+        e.style.cursor = "default";
     }
-    window.onload = function () {
-        disableSelection(document.body)
-    };
+
+    disableSelection(document.body);
 
     window.addEventListener("keydown", function (e) {
-        if (e.ctrlKey && (e.which == 65 || e.which == 66 || e.which == 67 || e.which == 70 || e.which == 73 || e.which == 80 || e.which == 83 || e.which == 85 || e.which == 86)) {
-            e.preventDefault()
+        if (e.ctrlKey && [65, 66, 67, 70, 73, 80, 83, 85, 86].includes(e.which)) {
+            e.preventDefault();
         }
     });
-    document.keypress = function (e) {
-        if (e.ctrlKey && (e.which == 65 || e.which == 66 || e.which == 70 || e.which == 67 || e.which == 73 || e.which == 80 || e.which == 83 || e.which == 85 || e.which == 86)) { }
-        return false
-    };
+
+    document.addEventListener("keypress", function (e) {
+        if (e.ctrlKey && [65, 66, 67, 70, 73, 80, 83, 85, 86].includes(e.which)) {
+            e.preventDefault();
+        }
+    });
+
     document.onkeydown = function (e) {
         e = e || window.event;
-        if (e.keyCode == 123 || e.keyCode == 18) {
-            return false
-        }
+        if (e.keyCode == 123 || e.keyCode == 18) return false;
     };
+
     document.oncontextmenu = function (e) {
-        var t = e || window.event;
-        var n = t.target || t.srcElement;
-        if (n.nodeName != "A") return false
+        var n = e.target || e.srcElement;
+        if (n.nodeName != "A") return false;
     };
+
     document.ondragstart = function () {
-        return false
+        return false;
     };
 });
